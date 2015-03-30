88bb295a4ee845291dabf5e06d413e120877439b
fabriciosoares <fabriciosoares@yahoo.com>
2015-03-30 02:34:03 -0300

    Inicio do Projeto FolhaPagamento

---
+++ src/folhaPagamento/Funcionario.java
@@ -0,0 +1,30 @@
+package folhaPagamento;
+
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	private double salario;
+	public Funcionario(String nome, String cpf, double salario) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.salario = salario;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return this.cpf;
+	}
+	public void setCPF(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSalario() {
+		return this.salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+}
